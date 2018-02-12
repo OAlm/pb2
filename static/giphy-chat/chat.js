7b8097ec1d7100473926c71216e848b8ffd25f1a
Olli Alm <olli.alm@metropolia.fi>
2018-02-12 15:44:12 +0200

    giphy with PB2 client lib

--- static/giphy-chat/chat.js
+++ static/giphy-chat/chat.js
@@ -1,82 +1,69 @@
 'use strict';
-/* global io $ document window */
+/* global $ document window PB2 */
 
-/**
- * @param {string} app_id the application identifier send to the server
- */
-function initSocket(app_id) {
-	console.log('using hostname: '+window.location.hostname);
-	const socket = io(window.location.hostname);
-	let sessionid;
-	// register room to the server
-	socket.on('connect', function() {
-		console.log('socket.io connected!');
-		sessionid = socket.io.engine.id;
-		socket.emit('app_id', app_id);
-	});
+function onMessage(data) {
+    let sender = data.json.client_name;
+    if (data.me) {
+        sender = 'you';
+    }
+    const LIMIT = 10;
+    $.ajax({
+        dataType: 'json',
+        url: 'https://api.giphy.com/v1/gifs/search',
+        data: {
+            'api_key': 'dc6zaTOxFJmzC',
+            'q': data.json.str,
+            'limit': LIMIT,
+        },
+        success: function(result) {
+            console.log(result);
+            let image = $('<img>');
+            image.attr('src', result.
+              data[Math.floor((Math.random() * LIMIT))].
+                  images.fixed_height.url);
+            $('#messages').append(image);
+        },
+    });
 
-	// on response
-	socket.on('message', function(data) {
-		let sender = data.socketid;
-		if(data.socketid === sessionid) {
-			sender = 'you';
-		}
-		console.log('message from '+sender+': '+JSON.stringify(data));
+    let paragraph = $('<p>');
+    paragraph.html(sender+' says "'+data.json.str+'"');
+    $('#messages').append(paragraph);
+}
 
-		const LIMIT = 10;
+function initPB() {
+    const pb2 = new PB2(window.location.hostname, 'giphy-chat-demo');
+    pb2.setReceiver(onMessage);
 
-		$.ajax({
-            dataType: 'json',
-            url: 'https://api.giphy.com/v1/gifs/search',
-            data: {
-                'api_key': 'dc6zaTOxFJmzC',
-                'q': data.str,
-				'limit': LIMIT,
-            },
-            success: function(result) {
-				console.log(result);
-				let image = $('<img>');
-				image.attr('src', result.data[Math.floor((Math.random() * LIMIT))].images.fixed_height.url);
-				$('#messages').append(image);
-			},
-		});
-		let paragraph = $('<p>');
-		paragraph.html(data.client_name+' says "'+data.str+'"');
-		$('#messages').append(paragraph);
-	});
-
-	// sending messages
-	$('#message').bind('keypress', function(e) {
-		const code = (e.keyCode ? e.keyCode : e.which);
-		if (code == 13 && socket) { // keyboard Enter
-			const msg = {};
-			msg.client_name = $('#name').val();
-			msg.app_id = app_id;
-			msg.time = Date.now();
-			msg.str = $('#message').val();
-			socket.json.emit('message', msg);
-			$('#message').val('');
+    // sending messages
+    $('#message').bind('keypress', function(e) {
+        const code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 13) { // keyboard Enter
+            const msg = {};
+            msg.client_name = $('#name').val();
+            msg.str = $('#message').val();
+            pb2.sendJson(msg);
+            $('#message').val('');
         }
-	});
+    });
 }
 
 /** @function */
 function initNameCheck() {
-	$('#message').prop('disabled', true);
-	$('#message').css({'background-color': 'grey'});
+    $('#message').prop('disabled', true);
+    $('#message').css({'background-color': 'grey'});
 
-	$('#name').on('change paste keyup', function() {
-		if($('#name').val()) {
-			$('#message').prop('disabled', false);
-			$('#message').css({'background-color': 'white'});
-		} else {
-			$('#message').prop('disabled', true);
-			$('#message').css({'background-color': 'grey'});
-		}
-	});
+    $('#name').on('change paste keyup', function() {
+        if ($('#name').val()) {
+            $('#message').prop('disabled', false);
+            $('#message').css({'background-color': 'white'});
+        } else {
+            $('#message').prop('disabled', true);
+            $('#message').css({'background-color': 'grey'});
+        }
+    });
 }
 
 $(document).ready(function() {
-	initNameCheck();
-	initSocket('chat_app_giphy');
+    initNameCheck();
+    initPB();
 });
